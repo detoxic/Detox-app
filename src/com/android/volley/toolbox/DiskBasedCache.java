95ce581b42d569cdf0d831861b8edd36410b5586
Jonathan Koren <jdkoren@gmail.com>
2013-06-01 14:19:39 -0700

    Setup Google Play Services

--- src/com/android/volley/toolbox/DiskBasedCache.java
+++ src/com/android/volley/toolbox/DiskBasedCache.java
@@ -16,11 +16,6 @@
 
 package com.android.volley.toolbox;
 
-import android.os.SystemClock;
-
-import com.android.volley.Cache;
-import com.android.volley.VolleyLog;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -35,6 +30,11 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+import android.os.SystemClock;
+
+import com.android.volley.Cache;
+import com.android.volley.VolleyLog;
 
 /**
  * Cache implementation that caches files directly onto the hard disk in the specified
--- src/com/hackforchange/android/detox/MainActivity.java
+++ src/com/hackforchange/android/detox/MainActivity.java
@@ -5,21 +5,58 @@ import android.util.Log;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
-import com.hackforchange.detox_app.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.plus.PlusClient;
 
-public class MainActivity extends SherlockFragmentActivity {
+public class MainActivity extends SherlockFragmentActivity implements ConnectionCallbacks, OnConnectionFailedListener {
 	private static final String TAG = "MainActivity";
+
+	private LocationClient mLocationClient;
+	private PlusClient mPlusClient;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mLocationClient = new LocationClient(this, this, this);
+		mLocationClient.connect();
+		
+		mPlusClient = new PlusClient.Builder(this, this, this).build();
+		mPlusClient.connect();
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		Log.d(TAG, "[onCreateOptionsMenu]");
-		return super.onCreateOptionsMenu(menu);
+		getSupportMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		Log.d(TAG, "[onConnectionFailed]");
+		
+	}
+
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		Log.d(TAG, "[onConnected]");
+		if (connectionHint != null) {
+			Log.d(TAG, connectionHint.toString());
+		} else {
+			Log.d(TAG, "connectionHint = null");
+		}
+		
+	}
+
+	@Override
+	public void onDisconnected() {
+		Log.d(TAG, "[onDisconnected]");
+		
 	}
 
 }
